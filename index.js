1ec5ff3e8051421283fc7e5c12d2e213bd359e09
bharding2 <b.harding@gmail.com>
2018-03-21 14:48:20 -0700

    new test player

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const sbork = require('./sbork');
 
 var testPlayer = {
-  height: 6.16,
-  weight: 213,
-  fortyYard: 4.35,
-  tenYard: 1.53,
-  benchPress: 18,
-  verticalJump: 45,
-  broadJump: 11.58,
-  threeCone: 7.06,
-  shortShuttle: 4.3,
-  intercept: 181.924227
+  height: 5.77,
+  weight: 177,
+  fortyYard: 4.5,
+  tenYard: 1.45,
+  benchPress: 15,
+  verticalJump: 40,
+  broadJump: 11,
+  threeCone: 6.56,
+  shortShuttle: 4.06
 };
 
-console.log(sbork.wr(testPlayer));
+// var testPlayer = {
+//   height: 6.16,
+//   weight: 213,
+//   fortyYard: 4.35,
+//   tenYard: 1.53,
+//   // benchPress: 18,
+//   verticalJump: 45,
+//   broadJump: 11.58,
+//   threeCone: 7.06,
+//   shortShuttle: 4.3
+// };
+
+console.log(sbork(testPlayer, 'cb'));
