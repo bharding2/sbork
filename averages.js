88a5cd6a93af676df7ab3db42f85d1400ce34237
bharding2 <b.harding@gmail.com>
2018-03-21 14:48:11 -0700

    positional averages

---
+++ averages.js
@@ -0,0 +1,189 @@
+module.exports = {
+  cb: {
+    height: 5.917, //
+    weight: 193.2, //
+    fortyYard: 4.47, //
+    tenYard: 1.53,
+    benchPress: 14.94, //
+    verticalJump: 36.26, //
+    broadJump: 10.16, //
+    threeCone: 6.88, //
+    shortShuttle: 4.17 //
+  },
+  // wr: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // },
+  // fs: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // },
+  // ss: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // },
+  // rb: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // },
+  // te: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // },
+  // qb: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // },
+  // p: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // },
+  // olb: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // },
+  // ilb: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // },
+  // fb: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // },
+  // de: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // },
+  // ot: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // },
+  // og: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // },
+  // oc: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // },
+  // dt: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // },
+  // nt: {
+  //   height: 6.16,
+  //   weight: 213,
+  //   fortyYard: 4.35,
+  //   tenYard: 1.53,
+  //   benchPress: 18,
+  //   verticalJump: 45,
+  //   broadJump: 11.58,
+  //   threeCone: 7.06,
+  //   shortShuttle: 4.3
+  // }
+};
