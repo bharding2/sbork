1abe1db1a2e84c2404e7df0aa0706e6e08d71622
bharding2 <b.harding@gmail.com>
2018-03-21 14:16:26 -0700

    figuring out some default values

--- sbork.js
+++ sbork.js
@@ -1,16 +1,14 @@
-module.exports = {
-  wr: function (player) {
-    var heightCoef = -4.592111;
-    var weightCoef = 0.361056;
-    var fortyYardCoef = -15.159259;
-    var tenYardCoef = -45.532307;
-    var benchPressCoef = 0.252199;
-    var verticalJumpCoef = 1.32729;
-    var broadJumpCoef = 9.557272;
-    var threeConeCoef = -5.985102;
-    var shortShuttleCoef = -18.731278;
-    var intercept = 181.924227;
+module.exports = function (player) {
+  var heightCoef = -4.592111;
+  var weightCoef = 0.361056;
+  var fortyYardCoef = -15.159259;
+  var tenYardCoef = -45.532307;
+  var benchPressCoef = 0.252199;
+  var verticalJumpCoef = 1.32729;
+  var broadJumpCoef = 9.557272;
+  var threeConeCoef = -5.985102;
+  var shortShuttleCoef = -18.731278;
+  var intercept = 181.924227;
 
-    return player.height * heightCoef + player.weight * weightCoef + player.fortyYard * fortyYardCoef + player.tenYard * tenYardCoef + player.benchPress * benchPressCoef + player.verticalJump * verticalJumpCoef + player.broadJump * broadJumpCoef + player.threeCone * threeConeCoef + player.shortShuttle * shortShuttleCoef + intercept;
-  }
+  return player.height * heightCoef + player.weight * weightCoef + player.fortyYard * fortyYardCoef + (player.tenYard || 1.527) * tenYardCoef + (player.benchPress || 30.175) * benchPressCoef + player.verticalJump * verticalJumpCoef + player.broadJump * broadJumpCoef + player.threeCone * threeConeCoef + player.shortShuttle * shortShuttleCoef + intercept;
 };
