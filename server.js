493fecee0335cb3141da6b44071e5b600ffae7b4
bharding2 <b.harding@gmail.com>
2018-09-04 19:31:16 -0700

    hooking up player schema

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const hapi = require('hapi');
 const mongoose = require('mongoose');
+const Player = require('./models/player');
 
 mongoose.connect('mongodb://sbork:sb0rkus3r@ds111492.mlab.com:11492/sbork');
 
