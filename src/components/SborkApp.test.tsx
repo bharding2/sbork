c63087e2e90592f78d7ba505d76520dd56350b2d
bharding2 <b.harding@gmail.com>
2020-03-09 10:02:27 -0700

    player table in SborkApp

--- src/components/SborkApp.test.tsx
+++ src/components/SborkApp.test.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react';
 import {shallow} from 'enzyme';
-import {SborkApp} from './SborkApp';
+import {SborkApp, samplePlayer} from './SborkApp';
+import {PlayerTable} from './PlayerTable';
 
 describe('SborkApp', () => {
-  it('renders a sbork app', () => {
-    const app = shallow(<SborkApp />);
+  it('should render a sbork app', () => {
+    const subject = shallowRender({});
 
-    expect(app.find('.SborkApp').exists()).toBe(true);
+    expect(subject.find('.SborkApp').exists()).toBe(true);
+  });
+
+  it('should render a PlayerTable', () => {
+    const subject = shallowRender({});
+
+    expect(subject.find(PlayerTable).exists()).toBe(true);
+    expect(subject.find(PlayerTable).prop('players')).toEqual([samplePlayer])
   });
 });
+
+interface OptionalProps {}
+
+const shallowRender = (props: OptionalProps) => {
+  return shallow(<SborkApp {...makeProps(props)} />)
+};
+
+const makeProps = (props: OptionalProps) => {
+  return {};
+};
--- src/components/SborkApp.tsx
+++ src/components/SborkApp.tsx
@@ -1,6 +1,45 @@
 import * as React from 'react';
+import {FunctionComponent, useState} from 'react';
+import {PlayerTable} from './PlayerTable';
 import './SborkApp.scss'
 
-export const SborkApp: React.FunctionComponent = () => {
-  return <div className="SborkApp">Sbork App</div>;
+export interface Player {
+  name: string;
+  position: string;
+  height: number;
+  weight: number;
+  fortyYard: number;
+  tenYard: number;
+  benchPress: number;
+  verticalJump: number;
+  broadJump: number;
+  threeCone: number;
+  shortShuttle: number;
+}
+
+export const samplePlayer: Player = {
+  name: 'sample player',
+  position: 'cb',
+  height: 5.77,
+  weight: 177,
+  fortyYard: 4.5,
+  tenYard: 1.45,
+  benchPress: 15,
+  verticalJump: 40,
+  broadJump: 11,
+  threeCone: 6.56,
+  shortShuttle: 4.06
+};
+
+export const SborkApp: FunctionComponent = () => {
+  const [players, setPlayers] = useState<Player[]>([samplePlayer]);
+
+  return (
+    <div className="SborkApp">
+      <div className="SborkApp-title">Sbork App</div>
+      <PlayerTable
+        players={players}
+      />
+    </div>
+  );
 };
